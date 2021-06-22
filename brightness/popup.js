2331935f7aa6865b806e9e20df441ee04d65443a
bexuyo <32695571+bexuyo@users.noreply.github.com>
2021-06-22 16:45:41 +0800

    Add files via upload

---
+++ brightness/popup.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 8 */
+
+let input = document.querySelector('input');
+
+input.addEventListener('change', e => setValue(e.target.value));
+
+async function setValue(value) {
+  await browser.storage.local.set({ value });
+}
+
+async function init() {
+  let { value } = browser.local.storage.get('value');
+  if (!value) {
+    value = 100;
+  }
+  input.value = value;
+  setValue(value);
+}
+
+init().catch(e => console.error(e));
